0d11848d9939da45fe95ab83e43e44119e43a9b6
Diovane Pacheti <negociospacheti@gmail.com>
2024-02-12 14:09:21 -0300

    :art: Add config routes tags

--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -6,4 +6,4 @@ tags_routes_bp = Blueprint('tags_routes', __name__)
 @tags_routes_bp.route('/create_tag', methods=["POST"])
 def create_tags():
     print(request.json)
-    return jsonify({"resp":"ok"}), 201
+    return jsonify({"resp": "ok"}), 201
