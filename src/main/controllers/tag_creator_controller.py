2aac466817c635ae65a16f8bc2d377a0cda324c8
Diovane Pacheti <negociospacheti@gmail.com>
2024-02-12 17:36:08 -0300

    :bug: Fix erro code

--- src/main/controllers/tag_creator_controller.py
+++ src/main/controllers/tag_creator_controller.py
@@ -4,21 +4,17 @@ from src.drivers.barcode_handler import BarcodeHandler
 
 class CreateTagController:
 
-    def __init__(self, barcode_handler: BarcodeHandler):
-        self.barcode_handler = barcode_handler
-
-    def create(self, product_code: str) -> Dict[str, Dict[str, int]]:
+    def create(self, product_code: str) -> Dict:
         path_from_tag = self.__create_tag(product_code)
         formatter_response = self.__format_response(path_from_tag)
-         return formatter_response
-
+        return formatter_response
 
     def __create_tag(self, product_code: str) -> str:
-        path_from_tag = self.barcode_handler.create.barcode(product_code)
+        barcode_handler = BarcodeHandler()
+        path_from_tag = barcode_handler.create_barcode(product_code)
         return path_from_tag
 
-
-    def __format_response(self, path_from_tag: str) -> Dict[str, Dict[str, int]]:
+    def __format_response(self, path_from_tag: str) -> Dict:
         return {
             "data": {
                 "type": "Tag Image",
