6ce221a3be2bfac3812591e79923a1d42b5c9521
Diovane Pacheti <negociospacheti@gmail.com>
2024-02-12 18:00:10 -0300

    :art: Finishing response

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,11 +1,17 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
+
 
 class TagCreatorView:
 
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
+
         body = http_request.body
         product_code = body.get("product_code")
-        print("passando pela view")
-        return HttpResponse(status_code=int(201), body=product_code)
+
+        formatter_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=201, body=formatter_response)
