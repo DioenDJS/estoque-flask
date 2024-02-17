825e6601db3011c34f3d399586664c48d7b176d7
Diovane Pacheti <negociospacheti@gmail.com>
2024-02-16 23:35:38 -0300

    :recycle: Refator host and port connect database

--- src/database/database.py
+++ src/database/database.py
@@ -1,17 +1,11 @@
 import peewee
 from dotenv import load_dotenv
 import os
-# import psycopg2
-#
-#
-# def get_connet_psycopg2():
-#     conn = psycopg2.connect(dbname="estoque_flask", user="user_test")
-#     return conn
 
 
 def get_connect():
     load_dotenv()
-    db = peewee.PostgresqlDatabase(os.getenv("DB_NAME"), user=os.getenv("DB_USER"), password=os.getenv("DB_PASSWORD"), host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"))
+    db = peewee.PostgresqlDatabase(os.getenv("DB_NAME"), user=os.getenv("DB_USER"), password=os.getenv("DB_PASSWORD"), host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT_DOCKER"))
     return db
 
 
