0053edb692e6bac884352973d3fc7d58c6802c44
Diovane Pacheti <negociospacheti@gmail.com>
2024-02-23 12:58:00 -0300

    :sparkles: Implements route user

---
+++ src/main/routes/user_routes.py
@@ -0,0 +1,25 @@
+from flask import request, jsonify, Blueprint
+from src.validators.user_creator_validator import user_creator_validator
+from src.views.http_types.http_request import HttpRequest
+from src.views.user_view.user_view import UserView
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
+
+user_routes_bp = Blueprint('user_routes', __name__)
+
+
+@user_routes_bp.route('/user/created', methods=["POST"])
+@jwt_required()
+def create_user():
+    resp = None
+    try:
+        user_view = UserView()
+        user_creator_validator(request)
+
+        http_request = HttpRequest(body=request.json)
+
+        resp = user_view.validate_and_create_user(http_request)
+    except Exception as e:
+        print(e)
+        resp = jsonify({"error": str(e)}), 500
+    return jsonify(resp.body), resp.status_code[0]
