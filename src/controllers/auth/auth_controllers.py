c94ec8be0a239dab58503deba327b08b2f81ec1f
Diovane Pacheti <negociospacheti@gmail.com>
2024-02-23 12:54:03 -0300

    :sparkles: Implementation auth and JWT

---
+++ src/controllers/auth/auth_controllers.py
@@ -0,0 +1,51 @@
+from src.main.domain.models.models import Users
+from src.database.database import get_connect
+from flask import jsonify
+from flask_jwt_extended import create_access_token, verify_jwt_in_request, get_jwt_identity
+from functools import wraps
+import bcrypt
+
+
+class AuthController:
+
+    def auth_token(self, username: str, password: str):
+        try:
+
+            with get_connect():
+                users = Users.select().where(Users.full_name == username)
+                user = [user.serialize() for user in users]
+                if not user:
+                    raise Exception("User not found!")
+
+                user_check_password = user[0]
+                storage_hashed_passqword = user_check_password.get('password')
+                storage_hashed_passqword_byte = storage_hashed_passqword.encode('utf-8')
+                password_match = bcrypt.checkpw(password.encode('utf-8'), storage_hashed_passqword_byte)
+
+                if password_match:
+                    identity_data = {
+                        'username': username,
+                        'roles': [user_check_password.get('role').get('nome')],
+                    }
+                    return create_access_token(identity=identity_data)
+                else:
+                    print("Password is incorrect.")
+        except Exception as e:
+            print(e)
+
+
+    def roles_required(*required_roles):
+        def decorator(fn):
+            @wraps(fn)
+            def wrapper(*args, **kwargs):
+                verify_jwt_in_request()
+                current_roles = get_jwt_identity().get('roles', [])
+
+                if any(role in current_roles for role in required_roles):
+                    return fn(*args, **kwargs)
+                else:
+                    return jsonify({"error": "Insufficient permissions"}), 403
+
+            return wrapper
+
+        return decorator
